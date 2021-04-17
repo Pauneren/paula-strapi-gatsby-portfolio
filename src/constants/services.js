9bd59dfa4f8131d284e0a1376a51634fe719f847
Paula Nerenberg <paulita.glam@gmail.com>
2021-04-17 17:58:11 +0000

    first commit

---
+++ src/constants/services.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { FaCode, FaSketch, FaAndroid } from "react-icons/fa"
+export default [
+  {
+    id: 1,
+    icon: <FaCode className="service-icon" />,
+    title: "web development",
+    text: `Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. `,
+  },
+  {
+    id: 2,
+    icon: <FaSketch className="service-icon" />,
+    title: "web design",
+    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+  },
+  {
+    id: 3,
+    icon: <FaAndroid className="service-icon" />,
+    title: "app design",
+    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+  },
+]
